4a3b787f234163121114048e0e56b04fd2a34cf9
Chitou Abdel Rodrigue <char.wilsons@gmail.com>
2026-04-10 23:21:54 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-import { initUI } from "./ui.js";
-import { renderCV } from "./preview.js";
-import { exportPDF } from "./export.js";
-
-initUI();
-renderCV();
-
-document.getElementById("download").addEventListener("click", exportPDF);
